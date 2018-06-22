75a7cd81ff1ce8ed024a796e82012e8e7c521603
Martin Ma <maang_ouc@163.com>
2018-06-23 00:10:05 +0800

    Initial commit

---
+++ src/BinaryTree/TestMainClass.java
@@ -0,0 +1,19 @@
+package BinaryTree;
+
+import java.util.List;
+
+public class TestMainClass {
+
+    public static void main(String args[]) {
+
+        Traversal traversal = new Traversal();
+
+        TreeNode root = new TreeNode(0);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+
+        List<Integer> preorderList = traversal.afterOrder(root);
+
+    }
+}
---
+++ src/BinaryTree/Traversal.java
@@ -0,0 +1,86 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Traversal {
+
+    List<Integer> preRes = new ArrayList<Integer>();
+
+    List<Integer> inRes = new ArrayList<>();
+
+    List<Integer> afterRes = new ArrayList<>();
+
+    List<List<Integer>> levelRes = new ArrayList<>();
+
+    public List<Integer> preOrder (TreeNode root) {
+
+        if (root == null) {
+            return preRes;
+        }
+        preRes.add(root.val);
+
+        preOrder(root.left);
+        preOrder(root.right);
+
+        return preRes;
+    }
+
+    public List<Integer> inOrder (TreeNode root) {
+        if (root == null) {
+            return inRes;
+        }
+
+        inOrder(root.left);
+        inRes.add(root.val);
+        inOrder(root.right);
+
+        return inRes;
+    }
+
+    public List<Integer> afterOrder(TreeNode root) {
+
+        if (root == null) {
+            return afterRes;
+        }
+        afterOrder(root.left);
+        afterOrder(root.right);
+        afterRes.add(root.val);
+
+        return afterRes;
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        TreeNode current = null;
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> levelList = new ArrayList<>();
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i ++) {
+                current = queue.poll();
+                levelList.add(current.val);
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+            levelRes.add(levelList);
+
+        }
+
+        return levelRes;
+
+    }
+}
