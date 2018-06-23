f09bf38d9166d3268e5f6ef58042db674627cd08
Martin Ma <maang_ouc@163.com>
2018-06-23 11:49:26 +0800

    add maxDepth solution

---
+++ src/BinaryTree/maxDepth.java
@@ -0,0 +1,28 @@
+package BinaryTree;
+
+public class maxDepth {
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return maximumDepth(root, 1);
+
+
+    }
+
+    int answer = 0;
+    private int maximumDepth(TreeNode root, int depth) {
+
+        if (root == null) {
+            return 0;
+        }
+        answer = Math.max(answer, depth);
+        maximumDepth(root.left, depth + 1);
+        maximumDepth(root.right, depth + 1);
+        return answer;
+
+    }
+}
