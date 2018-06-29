1877b142b0a158c887c32ad9375c9a4d73e12919
mma208 <maang_ouc@163.com>
2018-06-29 17:14:22 +0800

    add a solution about validation of BST

---
+++ src/BinaryTree/validBST.java
@@ -0,0 +1,29 @@
+package BinaryTree;
+
+public class validBST {
+
+
+    public boolean isValidBST(TreeNode root)  {
+        return isBSTUtil(root, Integer.MIN_VALUE,
+                Integer.MAX_VALUE);
+    }
+
+    /* Returns true if the given tree is a BST and its
+      values are >= min and <= max. */
+    boolean isBSTUtil(TreeNode node, int min, int max)
+    {
+        /* an empty tree is BST */
+        if (node == null)
+            return true;
+
+        /* false if this node violates the min/max constraints */
+        if (node.val < min || node.val > max)
+            return false;
+
+        /* otherwise check the subtrees recursively
+        tightening the min/max constraints */
+        // Allow only distinct values
+        return (isBSTUtil(node.left, min, node.val-1) &&
+                isBSTUtil(node.right, node.val+1, max));
+    }
+}
