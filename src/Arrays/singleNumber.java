28d5b07e6200c53cbd2c47fe338574b10e45fb93
Martin Ma <maang_ouc@163.com>
2018-07-03 01:48:37 +0800

    add some Array solution

---
+++ src/Arrays/singleNumber.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class singleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+
+    }
+}
